0fb5628393cce4aac473807cfe6d8f40be6080b7
Jacob Asper <jacobasper191@gmail.com>
2023-12-19 22:29:45 -0500

    Add basic route

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
+use std::net::SocketAddr;
+
+use axum::{response::Html, routing::get, serve, Router};
+use tokio::net::TcpListener;
+
 #[tokio::main]
 async fn main() {
-	println!("Hello, world!");
+	let routes_hello = Router::new().route(
+		"/hello",
+		get(|| async { Html("<h1>hello from the server</h1>") }),
+	);
+
+	let address = SocketAddr::from(([127, 0, 0, 1], 8080));
+	let listener = TcpListener::bind(address)
+		.await
+		.unwrap();
+	println!("Listening on http://{address}");
+
+	serve(listener, routes_hello.into_make_service())
+		.await
+		.unwrap();
 }
