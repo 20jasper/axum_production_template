6be0a35b96efacdfa53945dad0baa629e71171fa
Jacob Asper <78604367+20jasper@users.noreply.github.com>
2023-12-28 20:40:39 -0500

    Path params (#6)
    * add path param route
    
    * Create groups router

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::net::SocketAddr;
 
-use axum::{extract::Query, response::Html, routing::get, serve, Router};
+use axum::{
+	extract::{Path, Query},
+	response::Html,
+	routing::get,
+	serve, Router,
+};
 use serde::Deserialize;
 use tokio::net::TcpListener;
 
@@ -16,9 +21,25 @@ async fn hello_handler(Query(HelloParams { name }): Query<HelloParams>) -> Html<
 	Html(format!("<h1>hello {name}</h1>"))
 }
 
+#[derive(Deserialize)]
+struct ByeParams {
+	name: String,
+	last_name: String,
+}
+
+async fn bye_handler(Path(ByeParams { name, last_name }): Path<ByeParams>) -> Html<String> {
+	Html(format!("<h1>bye {name} {last_name}</h1>"))
+}
+
+fn greetings_routes() -> Router {
+	Router::new()
+		.route("/hello", get(hello_handler))
+		.route("/bye/:name/:last_name", get(bye_handler))
+}
+
 #[tokio::main]
 async fn main() {
-	let routes_hello = Router::new().route("/hello", get(hello_handler));
+	let routes = Router::new().merge(greetings_routes());
 
 	let address = SocketAddr::from(([127, 0, 0, 1], 8080));
 	let listener = TcpListener::bind(address)
@@ -26,7 +47,7 @@ async fn main() {
 		.unwrap();
 	println!("Listening on http://{address}");
 
-	serve(listener, routes_hello.into_make_service())
+	serve(listener, routes.into_make_service())
 		.await
 		.unwrap();
 }
